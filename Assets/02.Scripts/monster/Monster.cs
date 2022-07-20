7f46aa64b01876e8361c246f95b71842093be1fd
sangindd <103929121+sangindd@users.noreply.github.com>
2022-07-20 11:33:55 +0900

    .
    .

--- Assets/02.Scripts/monster/Monster.cs
+++ Assets/02.Scripts/monster/Monster.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using Photon.Pun;
 using UnityEngine.UI;
 
-public class Monster : MonoBehaviourPun
+public class Monster : MonoBehaviourPun, IPunObservable
 {
 
     public PlayerStat playerStat;
@@ -78,7 +78,18 @@ public class Monster : MonoBehaviourPun
 
 
     }
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
 
+        if (stream.IsWriting)
+        {
+            stream.SendNext(curHealth);
+        }
+        else
+        {
+            curHealth = (float)stream.ReceiveNext();
+        }
+    }
     [PunRPC]
     public virtual void MonsterRespawn()
     {
