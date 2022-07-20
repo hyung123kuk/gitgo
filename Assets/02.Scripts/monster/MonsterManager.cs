9b95d3101581900898426c52102f3be97a8bd793
hyungkuk <im06@naver.com>
2022-07-20 17:10:36 +0900

    .
    ..

--- Assets/02.Scripts/monster/MonsterManager.cs
+++ Assets/02.Scripts/monster/MonsterManager.cs
@@ -25,7 +25,7 @@ public class MonsterManager : MonoBehaviourPunCallbacks
     public GameObject[] Skeleton;
     public GameObject TurtleSlime;
     public GameObject Golem;
-
+    public GameObject ResetBox;
     public static MonsterManager monsterManager;
 
     private void Awake()
@@ -42,13 +42,18 @@ public class MonsterManager : MonoBehaviourPunCallbacks
 
     public override void OnMasterClientSwitched(Player newMasterClient)
     {
+        
         Debug.Log("마스터클라이언트 나감");
-        Generate();
+        ResetBox.SetActive(true);
+
+        Invoke("Generate", 0.1f);
+       
     }
 
 
     public void Generate()  //Ǯ�� ����
     {
+        ResetBox.SetActive(false);
         GameObject SlimePools = new GameObject("Slime Pool");
         GameObject BlueSlimePools = new GameObject("BlueSlime Pool");
         GameObject GoblinPools = new GameObject("Goblin Pool");
